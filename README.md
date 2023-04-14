53d0231f82413e4e057c44451aa699a8dbba9be4
Katarzyna Kuśmierczyk <kusmierczyk.kasia1101@gmail.com>
2023-04-14 22:11:25 +0200

    finish combined file

--- README.md
+++ README.md
@@ -1,5 +1,45 @@
 Small project for getting useful data from ITL2023 competition
 
+# itl_stats.rb
+Single file for all options
+Usage: itl_stats [options]
+    -m, --mode mode                  Available options: users, stats (default - users)
+    -k, --kind kind                  Kind of user list you want to get or kind of stats you want to check. 
+                                     Available options for users: regions, list
+                                     Available options for stats: passes, levels, levelstop, points
+    -f, --file file                  Name of file you want to use as input
+    -i, --id id                      Id of itl user you want to check stats for
+    -n, --nick nick                  Nick of itl user you want to check stats for
+    -r, --regions regions            List of regions - used when mode and kind are set to 'users' and 'list'
+
+## Example of uses
+`ruby itl_stats.rb -m users -k regions`
+Outputs list of all itl users in format `{"gs_id":174237, "id":604, "name":"Reina0111", "region":"Poland"}`
+
+`ruby itl_stats.rb -m users -k list -r "Poland, United Kingdom" -f users.txt`
+Outputs list of itl users from selected regions from list given as file input
+
+`ruby itl_stats.rb -m stats -k passes -i 604`
+Outputs total passes of player with given id
+`ruby itl_stats.rb -m stats -k passes -f users.txt`
+Outputs total passes for all players from list given as file input
+
+`ruby itl_stats.rb -m stats -k levels -nick Reina0111`
+Outputs number of passed songs on each level for given player
+`ruby itl_stats.rb -m stats -k levels -f users.txt`
+Outputs number of passed songs on each level for all players from list given as file input
+
+`ruby itl_stats.rb -m stats -k levelsTop --nick Reina0111`
+Outputs top 75 passed songs divided by level for given player
+`ruby itl_stats.rb -m stats -k levelsTop -f users.txt`
+Outputs top 75 passed songs divided by level for all players from list given as file input
+
+`ruby itl_stats.rb -m stats -k points -i 604`
+Outputs total points and ranking points for player with given id
+`ruby itl_stats.rb -m stats -k points -f users.txt`
+Outputs total points and ranking points for all players from list given as file input
+
+
 # users_list.rb
 This file is used to get list of all users participating in competition from https://itl2023.groovestats.com page with additional info about users regions
 
---
+++ itl_stats.rb
@@ -0,0 +1,205 @@
+require 'optparse'
+require 'open-uri'
+require 'json'
+
+MODE_OPTIONS = ["users", "stats"]
+STATS_KIND_OPTIONS = ["passes", "levels", "levelstop", "points"]
+USERS_KIND_OPTIONS = ["regions", "list"]
+ALL_KINDS = USERS_KIND_OPTIONS + STATS_KIND_OPTIONS
+
+@levels = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+
+def parseOptions
+  options = {}
+  begin
+    OptionParser.new do |opt|
+      opt.on('-m', '--mode mode', "Available options: #{MODE_OPTIONS.join(', ')}") { |o| options[:mode] = o if MODE_OPTIONS.include?(o.downcase) }
+      opt.on('-k', '--kind kind', 
+        "Kind of user list you want to get or kind of stats you want to check. Available options: #{ALL_KINDS.join(', ')}") { |o| options[:kind] = o.downcase if ALL_KINDS.include?(o.downcase) }
+      opt.on('-f', '--file file', "Name of file you want to use as input") { |o| options[:file] = o }
+      opt.on('-i', '--id id', "Id of itl user you want to check stats for") { |o| options[:id] = o }
+      opt.on('-n', '--nick nick', "Nick of itl user you want to check stats for") { |o| options[:nick] = o }
+      opt.on('-r', '--regions regions', "List of regions - used for 'users' mode and 'list' kind") { |o| options[:regions] = o.split(',').map { |r| r.strip() } }
+    end.parse!
+  rescue OptionParser::InvalidOption => e
+    puts e
+    options = {}
+  end
+
+  options[:mode] = MODE_OPTIONS[0] if !options[:mode]
+
+  if options[:mode] && options[:kind]
+    options
+  else
+    {}
+  end
+end
+
+def get_leaderboard(my_list = nil)
+  list = nil
+  URI.open('https://itl2023.groovestats.com/api/entrant/leaderboard') do |uri|
+      list = JSON.parse(uri.read)["data"]["leaderboard"]
+  end
+
+  if my_list
+    ids = my_list.map { |user| user["id"] }
+    list = list.select { |user| ids.include?(user["id"]) }
+  end
+
+  list
+end
+
+def parse_file_or_get_user(file, id, nick)
+  list = []
+  return get_user(id, nick) if !file
+
+  File.open(file).each do |line|
+      list << JSON.parse(line)
+  end
+
+  list
+end
+
+def get_user(id, nick)
+  return nil if !id && !nick
+
+  if !id
+      list = nil
+      URI.open('https://itl2023.groovestats.com/api/entrant/leaderboard') do |uri|
+          list = JSON.parse(uri.read)["data"]["leaderboard"]
+      end
+      user = list.select { |user| user["name"] == nick }.first
+      if user
+          id = user["id"]
+      else
+          puts "Nie znaleziono gracza - upewnij się, że podałeś taki sam nick jaki widnieje na stronie itl2023.groovestats.com"
+          return []
+      end
+  end
+  
+  user = nil
+
+  URI.open("https://itl2023.groovestats.com/api/entrant/#{id}") do |uri|
+      user = JSON.parse(uri.read)["data"]
+  end
+
+  [user]
+end
+
+def users_mode(options)
+  list = get_leaderboard()
+  
+  list.map! { |user| { gs_id: user["membersId"], id: user["id"], name: user["name"] } }
+    
+  users_list = parse_file_or_get_user(options[:file], options[:id], options[:nick])
+  users_list_ids = users_list&.map { |user| user["gs_id"] }
+
+  case options[:kind]
+  when "regions"
+    existing_ids = list.map { |user| user[:gs_id] }.select { |id| !users_list_ids.include?(id) }
+    full_list = []
+    regions = []
+    
+    while existing_ids.count > 0
+      list_to_check = list.select { |user| existing_ids.include?(user[:gs_id]) }
+      existing_ids = []
+  
+      list_to_check.each do |user|
+        begin
+          URI.open("https://groovestats.com/index.php?page=profile&id=#{user[:gs_id]}") do |uri|
+            page = uri.read
+            region = page[page.index('href="?page=regions')..-1]
+            region = region[region.index('>')+1..region.index('<')-1]
+            user[:region] = region
+            regions << region
+          end
+          full_list << user
+          puts user.to_json
+        rescue Net::OpenTimeout
+          existing_ids << user[:gs_id]
+        end
+      end
+    end    
+  when "list"
+    regions = options[:regions]
+
+    region_list = users_list&.select { |user| regions.include?(user["region"]) } if regions
+    region_list&.each do |user|
+      puts user.to_json
+    end
+  end
+end
+
+def stats_mode(options)
+  user_list = parse_file_or_get_user(options[:file], options[:id], options[:nick])
+  multi = user_list.count > 1
+
+  if multi
+    case options[:kind]
+    when "levels", "levelstop"
+      puts "#{" " * 16}#{@levels.map { |lvl| "| " + lvl.to_s.rjust(3, " ") + " " }.join }"
+      puts "#{"-" * 16}#{@levels.map { |_| "+" + "-"*5 }.join}"
+    when "points"
+      puts "#{" "*16}|ranking| total"
+    end
+  end
+
+  user_list.each do |user|
+    user = get_user(user["id"], nil)[0] if !user["entrant"]
+    case options[:kind]
+    when "passes"
+      songs = user["topScores"]
+      sum = songs.sum { |song| song["totalPasses"].to_i }
+      puts "#{user["entrant"]["name"].to_s.rjust(15, " ")} - passes: #{sum.to_s.rjust(3, " ")}"
+    when "levels"
+      user_songs = user["topScores"]
+      songs = user["charts"]
+      user_songs.map! { |u_song| songs.find { |song| song["hash"] == u_song["chartHash"] } }
+      grouped_levels = user_songs.group_by { |song| song["meter"] }
+      
+      if multi
+        levels_count = @levels.map { |lvl| grouped_levels[lvl] ? grouped_levels[lvl].count : 0 }
+        puts "#{user["entrant"]["name"].to_s[0..14].rjust(15, " ")} | #{levels_count.map { |lvl| lvl.to_s.rjust(3, " ") }.join(" | ")}"
+      else
+        puts "Songs by level for user #{user["entrant"]["name"]}"
+        puts grouped_levels.sort.map { |k, v| "Level #{k.to_s.rjust(2, " ")}: #{v.count.to_s.rjust(3, " ")}" }
+      end
+    when "levelstop"
+      user_songs = user["topScores"].sort_by { |song| song["points"] }.reverse[0..74]
+      songs = user["charts"]
+      user_songs.map! { |u_song| songs.find { |song| song["hash"] == u_song["chartHash"] } }
+      grouped_levels = user_songs.group_by { |song| song["meter"] }
+      
+      if multi
+        levels_count = @levels.map { |lvl| grouped_levels[lvl] ? grouped_levels[lvl].count : 0 }
+        puts "#{user["entrant"]["name"].to_s[0..14].rjust(15, " ")} | #{levels_count.map { |lvl| lvl.to_s.rjust(3, " ") }.join(" | ")}"
+      else
+        puts "Top 75 songs by level for user #{user["entrant"]["name"]}"
+        puts grouped_levels.sort.map { |k, v| "Level #{k.to_s.rjust(3, " ")}: #{v.count.to_s.rjust(3, " ")}" }
+      end
+    when "points"
+      if multi
+        puts "#{user["entrant"]["name"].to_s[0..14].rjust(15, " ")} |#{user["entrant"]["rankingPoints"].to_s.rjust(7, " ") }|#{user["entrant"]["totalPoints"].to_s.rjust(7, " ") }"
+      else
+        puts "          User: #{user["entrant"]["name"].to_s[0..14].rjust(15, " ")}"
+        puts "Ranking Points: #{user["entrant"]["rankingPoints"].to_s[0..14].rjust(15, " ")}"
+        puts "  Total Points: #{user["entrant"]["totalPoints"].to_s[0..14].rjust(15, " ")}"
+      end
+    end
+  end
+end
+
+def main
+  options = parseOptions()
+
+  return if !options || options == {}
+
+  case options[:mode]
+  when "users"
+    users_mode(options)
+  when "stats"
+    stats_mode(options)
+  end
+end
+
+main
